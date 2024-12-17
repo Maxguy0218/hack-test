dd247d5f258d56070c0cf83415b0816be27a0d02
Maxguy0218 <118455375+Maxguy0218@users.noreply.github.com>
2024-12-17 23:53:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ st.title("Demand To Talent")
 model, data, label_encoders, feature_columns = load_and_train_model()
 
 # Load the user-provided CSV file
-uploaded_file = 'selected_demand.csv'
+uploaded_file = 'sample_demand_data.csv'
 demand_data = pd.read_csv(uploaded_file)
 
 # User selects ID from dropdown
